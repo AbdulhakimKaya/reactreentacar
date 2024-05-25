0d84480849166e4fe26241ae809f5aa61982a205
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-05-25 17:32:47 +0300

    genel yapı v3 (tema rengi belirlendi)

---
+++ src/mock/vehicleDetailMockData.ts
@@ -0,0 +1,37 @@
+export const VehicleDetailMockData = [
+    {
+        key: '1',
+        label: 'Marka',
+        children: 'Volvo',
+    },
+    {
+        key: '2',
+        label: 'Model',
+        children: 'XC90 Recharge',
+    },
+    {
+        key: '3',
+        label: 'Renk',
+        children: 'Kristal Beyaz',
+    },
+    {
+        key: '4',
+        label: 'Vites',
+        children: 'Otomatik',
+    },
+    {
+        key: '5',
+        label: 'Yıl',
+        children: '2024',
+    },
+    {
+        key: '6',
+        label: 'Yakıt',
+        children: 'Elektrikli/Benzinli',
+    },
+    {
+        key: '7',
+        label: 'Plaka',
+        children: '21 ASD 2024',
+    },
+]
